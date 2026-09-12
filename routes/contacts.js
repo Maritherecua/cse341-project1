4c9982257f4250892aa3ec4e28dae9c957ca765a
Maritherecua <mrtrsarroyo@yahoo.com>
2026-09-12 15:55:22 -0700

    Test: Modified server.js and requests.rest to update middleware order and add contactId variable.

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,18 +3,147 @@ const router = express.Router();
 //Import the contacts controller or database query
 const contactsController = require('../controllers/contacts');
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Contact:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - favoriteColor
+ *         - birthday
+ *       properties:
+ *         _id:
+ *           type: string
+ *           example: 6a9b975f4a1a63c12728e9a9
+ *         firstName:
+ *           type: string
+ *           example: Maria
+ *         lastName:
+ *           type: string
+ *           example: Arroyo
+ *         email:
+ *           type: string
+ *           format: email
+ *           example: maria@example.com
+ *         favoriteColor:
+ *           type: string
+ *           example: green
+ *         birthday:
+ *           type: string
+ *           format: date
+ *           example: 1995-06-15
+ */
+
 // GET all contacts/root route
+/**
+ * @openapi
+ * /contacts:
+ *   get:
+ *     summary: Get all contacts
+ *     responses:
+ *       200:
+ *         description: A list of contacts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Contact'
+ *       500:
+ *         description: Unable to retrieve contacts
+ */
 router.get('/', contactsController.getAll);
 
 // GET single contact by ID
+/**
+ * @openapi
+ * /contacts/{id}:
+ *   get:
+ *     summary: Get a contact by ID
+ *     parameters:
+ *       - $ref: '#/components/parameters/ContactId'
+ *     responses:
+ *       200:
+ *         description: The contact
+ *       404:
+ *         description: Contact not found
+ *       500:
+ *         description: Invalid contact ID or server error
+ */
 router.get('/:id', contactsController.getSingle);
 // POST create a new contact
+/**
+ * @openapi
+ * /contacts:
+ *   post:
+ *     summary: Create a contact
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Contact'
+ *     responses:
+ *       201:
+ *         description: Contact created
+ *       500:
+ *         description: Unable to create contact
+ */
 router.post('/', contactsController.create);
 
 // PUT update a contact by ID
+/**
+ * @openapi
+ * /contacts/{id}:
+ *   put:
+ *     summary: Update a contact by ID
+ *     parameters:
+ *       - $ref: '#/components/parameters/ContactId'
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Contact'
+ *     responses:
+ *       200:
+ *         description: Contact updated
+ *       404:
+ *         description: Contact not found
+ *       500:
+ *         description: Invalid contact ID or server error
+ */
 router.put('/:id', contactsController.update);
 
 // DELETE a contact by ID
+/**
+ * @openapi
+ * /contacts/{id}:
+ *   delete:
+ *     summary: Delete a contact by ID
+ *     parameters:
+ *       - $ref: '#/components/parameters/ContactId'
+ *     responses:
+ *       200:
+ *         description: Contact deleted
+ *       404:
+ *         description: Contact not found
+ *       500:
+ *         description: Invalid contact ID or server error
+ * components:
+ *   parameters:
+ *     ContactId:
+ *       name: id
+ *       in: path
+ *       required: true
+ *       schema:
+ *         type: string
+ *       example: 6a9b975f4a1a63c12728e9a9
+ */
 router.delete('/:id', contactsController.delete);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,32 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 const mongodb = require('./data/database');
 
 const port = process.env.PORT || 3000;
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Contacts API',
+      version: '1.0.0',
+      description: 'API for managing contacts.'
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`
+      }
+    ]
+  },
+  apis: ['./routes/*.js']
+});
 
 //Import and use the router midleware
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 const routes = require('./routes');
 app.use('/', routes);
 //Initialize the database and start the server
